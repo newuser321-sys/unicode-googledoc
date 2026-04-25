dc47d2e6a28a76a2f7d78a053a8229bb71339294
newuser321-sys <martinmudaliar2005@gmail.com>
2026-04-25 16:10:26 +0530

    commit #2

--- GoogleDocParse.cpp
+++ GoogleDocParse.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <string>
+#include <curl/curl.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, string* output) {
+    size_t totalSize = size * nmemb;
+    output->append((char*)contents, totalSize);
+    return totalSize;
 }
 
+string fetchHTML(const string& url) {
+    CURL* curl;
+    string response;
 
+    curl = curl_easy_init();
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+
+    return response;
+}
+
+string get_td(string tr) {
+    size_t pos = 0;
+    while (tr.find("<td>", pos) && pos != string::npos) {
+
+    }
+}
+
+int main() {
+    string url = "https://docs.google.com/document/d/e/2PACX-1vSvM5gDlNvt7npYHhp_XfsJvuntUhq184By5xO_pA4b_gCWeXb6dM6ZxwN8rE6S4ghUsCj2VKR21oEP/pub";
+
+    string html = fetchHTML(url);
+
+    while(ht)
+    html.find("<td>")
+
+
+
+
+
+    system("pause > 0");
+
+    return 0;
+}
